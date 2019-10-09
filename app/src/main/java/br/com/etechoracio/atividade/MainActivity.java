a13c0dbcb9d2888dd20100da6915a13e7157e98a
Raul e Sarah <raulglitzk26@gmail.com>
2019-10-09 20:12:25 -0300

    Exercicio 5

--- app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener{
+public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener, AdapterView.OnItemLongClickListener {
 
     private boolean insertMode;
     private ItemAdapter adapter;
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements CustomDialog.Item
         getMenuInflater().inflate(R.menu.ic_menu_add,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
